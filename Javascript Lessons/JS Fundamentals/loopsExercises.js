a4a87ec84a0f1acb09803077efcfdf391826dbd2
Stephen Clark <sdclarkie@gmail.com>
2019-07-07 14:26:20 -0400

    Update loopsExercises.js

--- Javascript Lessons/JS Fundamentals/loopsExercises.js
+++ Javascript Lessons/JS Fundamentals/loopsExercises.js
@@ -1,5 +1,6 @@
 /* Problems
-The output of the problems is written on a single line for brevity; your solutions will output each value on a new line like in the example above.
+The output of the problems is written on a single line for brevity; 
+your solutions will output each value on a new line.
 
 1. 0 100 200 300 400 500 600 700 800 900 1000
 2. 1 2 4 8 16 32 64 128
